4103305eea90c9230c4c21312f5946539d7ff761
sazan10 <33653716+sazan10@users.noreply.github.com>
2024-09-18 01:10:49 +0545

    Create simple_client.c

---
+++ simple_client.c
@@ -0,0 +1,12 @@
+#include "dr_api.h"
+
+void dr_init(client_id_t id) {
+    instr_t *instr;
+    instr = INSTR_CREATE_add_vector(drcontext,
+                                    opnd_create_reg(DR_REG_V0),  // Incorrect
+                                    opnd_create_reg(DR_REG_V1),  // Incorrect
+                                    opnd_create_reg(DR_REG_V2),  // Incorrect
+                                    OPND_CREATE_SINGLE());
+
+    dr_fprintf(STDERR, "Created instruction\n");
+}
